725cbf11aad6179238d8b68a2cdc80119f34c012
pink10000 <kyletrinh1223@gmail.com>
2024-10-06 15:07:32 -0700

    initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import ConcreteSemanticsLean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package "concrete-semantics-lean" where
+  version := v!"0.1.0"
+
+lean_lib «ConcreteSemanticsLean» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe "concrete-semantics-lean" where
+  root := `Main
