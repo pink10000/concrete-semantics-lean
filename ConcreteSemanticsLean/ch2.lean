d73fca4d38b70dd6c6299e2766c78a2217adb14d
pink10000 <kyletrinh1223@gmail.com>
2024-10-13 14:25:21 -0700

    define count for 2.3, need to discuss importing libraries

--- ConcreteSemanticsLean/ch2.lean
+++ ConcreteSemanticsLean/ch2.lean
@@ -1,10 +1,31 @@
 import ConcreteSemanticsLean.cslib
+import Init.Prelude
 
 -- 2.1
 
 -- 2.2
 
 -- 2.3
+def count [DecidableEq X] (elm : X) (l : mylist X) (occ : Nat) : Nat :=
+  match l with
+  | [] => occ
+  | a :: l' => if elm = a then count elm l' (occ + 1) else count elm l' occ
+
+#eval count 1 (1 :: 2 :: 1 :: []) 0 -- 2
+#eval count 3 (1 :: 2 :: 1 :: []) 0 -- 0
+#eval count type1.A type1_list 0 -- 1
+
+-- someone should try to find  way to remove the need for DecidableEq
+theorem count_x_le_len [DecidableEq X] : ∀ (elm : X) (l : mylist X), count elm l 0 ≤ len l := by
+  intros elm l
+  induction l
+  . simp [count, len]
+  . simp [count, len]
+    sorry
+
+
+
+
 
 -- 2.4
 
--- ConcreteSemanticsLean/cslib.lean
+++ ConcreteSemanticsLean/cslib.lean
@@ -5,33 +5,49 @@ def sample := "sample"
 
 section cslib
 
-  inductive mylist (X : Type) where
-  | nil
-  | cons (a : X) (l : mylist X)
-  deriving Repr
+inductive mylist (X : Type) where
+| nil
+| cons (a : X) (l : mylist X)
+deriving Repr
 
-  set_option quotPrecheck false in
-  notation x " :: " l => (mylist.cons x l)
-  notation "[]" => mylist.nil
+set_option quotPrecheck false in
+notation x " :: " l => (mylist.cons x l)
+notation "[]" => mylist.nil
 
-  #check mylist.cons 1 mylist.nil
+#check mylist.cons 1 mylist.nil
+def append (l : mylist X) (a : X) : (mylist X) := a :: l
 
-  def append (l : mylist X) (a : X) : (mylist X) := a :: l
+#eval append [] 1
+#check append (1 :: []) 2
 
-  #eval append [] 1
-  #check append (1 :: []) 2
+def reverse (l : mylist X) (acc : mylist X) : (mylist X) :=
+  match l with
+  | mylist.nil => acc
+  | a :: l' => reverse l' (a :: acc)
 
-  def reverse (l : mylist X) (acc : mylist X) : (mylist X) :=
-    match l with
-    | mylist.nil => acc
-    | a :: l' => reverse l' (a :: acc)
+#check reverse [] []
+#eval reverse (1 :: []) []
+#eval reverse (2 :: 1 :: []) []
 
-  #check reverse [] []
-  #eval reverse (1 :: []) []
-  #eval reverse (2 :: 1 :: []) []
+example : reverse (1 :: 2 :: 3 :: []) [] = 3 :: 2 :: 1 :: [] := by rfl
 
-  example : reverse (1 :: 2 :: 3 :: []) [] = 3 :: 2 :: 1 :: [] := by rfl
+def len (l : mylist X) : Nat :=
+  match l with
+  | mylist.nil => 0
+  | _ :: l' => 1 + len l'
 
-  #check 1 :: mylist.nil
+#eval len (1 :: 2 :: 3 :: []) -- 3
+#eval len ([] : mylist Nat) -- 0
+-- note, lean doesn't know what type [] is since we've told it to be implicit
+
+inductive type1 where
+| A
+| B
+| C
+deriving Repr, DecidableEq -- this is kinda insane that you need this
+-- lean straight up has no idea if the inductive types can be equal to itself or not
+-- adding DecidableEq tells lean "hey, you can check if these are equal or not"
+
+def type1_list := (type1.A :: type1.B :: type1.C :: [])
 
 end cslib
