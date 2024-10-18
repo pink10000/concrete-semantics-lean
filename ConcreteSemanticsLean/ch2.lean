fa23d1418b0fec4ee22156ed9b1bdc178729efba
Warren <warren@ucsd.edu>
2024-10-17 21:36:20 -0700

    do 2.1 and 2.3

--- ConcreteSemanticsLean/ch2.lean
+++ ConcreteSemanticsLean/ch2.lean
@@ -3,7 +3,10 @@ import Init.Prelude
 import Mathlib.Tactic.Linarith
 
 -- 2.1
-
+#eval 1 + (2: Nat) -- 3
+#eval 1 + (2: Int) -- 3
+#eval 1 - (2: Nat) -- 0
+#eval 1 - (2: Int) -- -1
 -- 2.2
 
 -- 2.3
@@ -32,6 +35,32 @@ theorem count_x_le_len [DecidableEq X] : ∀ (elm : X) (l : mylist X), count elm
 
 
 -- 2.4
+def snoc: mylist X -> X -> mylist X
+  | mylist.nil, b => b :: mylist.nil
+  | a :: l, b => a :: (snoc l b)
+
+#eval snoc (1::2::3::mylist.nil) 1 -- correctly appends to end
+
+def rev': mylist X -> mylist X
+  | mylist.nil => mylist.nil
+  | a :: l => snoc (rev' l) a
+
+#eval rev' (1::2::3::mylist.nil) -- correctly reverses
+
+-- [1] need to show reversing a right-append (snoc) on list l is left-appending elem with list l reversed
+theorem rev_snoc_is_left_app_rev: ∀ (l: mylist X) (elem: X), rev' (snoc l elem) = elem :: rev' l := by
+  intro l elem
+  induction l
+  case nil => rfl
+  case cons elem l' ih => simp [rev']; rw [ih]; simp [snoc]
+
+theorem rev_involutive: ∀ l: mylist X, rev' (rev' l) = l := by
+  intro l
+  induction l
+  case nil => rfl
+  case cons a l' ih =>
+    simp [rev'] -- [1] wants rev' (snoc (rev' l') a) = mylist.cons a l'
+    rw [rev_snoc_is_left_app_rev, ih]
 
 -- 2.5
 
