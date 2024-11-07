db3024d1e76cbcfae9265488c6340ed0b23095e5
pink10000 <kyletrinh1223@gmail.com>
2024-11-06 20:22:39 -0800

    some ch4.5

---
+++ ConcreteSemanticsLean/ch4_5.lean
@@ -0,0 +1,69 @@
+import ConcreteSemanticsLean.cslib
+import Init.Prelude
+import Mathlib.Tactic.Linarith
+import Mathlib.Tactic.Ring
+import Mathlib.Tactic.Basic
+
+/- Ch4.5.1 : An Example: Even Numbers -/
+section ch4_5_1
+
+  inductive ev : ℕ → Prop :=
+  | ev0   : ev 0
+  | evSS  : ev n → ev (n + 2)
+  open ev
+
+  theorem ev_4 : ev 4 := evSS (evSS ev0)
+  example : ev 0 → ev (0 + 2) → ev ((0 + 2) + 2) = ev 4 := by simp
+
+  /- Rule Induction -/
+  def evn (n : ℕ) :=
+    match n with
+    | 0 => True
+    | Nat.succ 0 => False
+    | Nat.succ (Nat.succ n) => evn n
+  #check evn
+
+  example : ev m → evn m := by
+    intro evm
+    induction evm -- rule induction
+    case ev0 => simp [evn]
+    case evSS m' _ evn_m =>
+      simp [ev, evn]; exact evn_m
+
+  example : ev m → ev (m - 2) := by
+    intro h
+    induction h <;> simp[ev]
+    . apply ev0
+    . assumption
+
+  example : ev n → ev (Nat.succ (Nat.succ n)) := by
+    intro evn; induction evn
+    . exact (evSS ev0)
+    . exact (evSS (by assumption))
+
+  example : ev (Nat.succ (Nat.succ (Nat.succ (Nat.succ 0)))) := evSS (evSS ev0)
+  example : ev (Nat.succ (Nat.succ (Nat.succ (Nat.succ 0)))) := by
+    apply evSS; apply evSS; apply ev0
+
+
+  lemma evn_is_ev : evn n → ev n := by
+    intro evn_n; induction n
+    . sorry
+    . sorry
+
+
+  /- Ch4.5.2 The Reflexive Transitive Closure -/
+  inductive star {α : Type} (r : α → α → Prop) : α → α → Prop
+  | refl {x : α} : star r x x
+  | step {x y z : α} (h₁ : r x y) (h₂ : star r y z) : star r x z
+
+
+  lemma star_trans : star r x y → star r y z → star r x z := by
+    intro rxy ryz
+    induction rxy
+    . exact ryz
+    . case step x' y' z' rx'y' ry'z' rz'z_ry'z =>
+      sorry
+
+
+end ch4_5_1
